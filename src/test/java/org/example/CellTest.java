64512f97fac3dbfb76174e0cd1bf715399beb1e8
aapoorv2 <abhyudaya.a_ftc@external.swiggy.in>
2024-01-26 20:18:30 +0530

    test for cell class isAlive method when cell is dead

--- src/test/java/org/example/CellTest.java
+++ src/test/java/org/example/CellTest.java
@@ -8,9 +8,15 @@ import static org.junit.jupiter.api.Assertions.*;
 class CellTest {
     @Test
     @DisplayName("Test if the cell is alive and returns true")
-    void testIsAliveMethod() {
+    void testIsAliveMethodWhenCellIsAlive() {
         Cell cell = new Cell(0, 0, State.ALIVE);
         assertTrue(cell.isAlive());
+    }
+    @Test
+    @DisplayName("Test if the cell is dead and returns false")
+    void testIsAliveMethodWhenCellIsDead() {
+        Cell cell = new Cell(0, 0, State.DEAD);
+        assertFalse(cell.isAlive());
     }
 
 }
