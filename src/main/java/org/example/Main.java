393bb8495652a0cf2a1b7746a0bfc70366509579
aapoorv2 <abhyudaya.a_ftc@external.swiggy.in>
2024-01-29 00:51:32 +0530

    modified main.java to take input from the user

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,19 @@
 package org.example;
+
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        Game game = new Game(3, 11, 37);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of Rows");
+        int rows = scanner.nextInt();
+        System.out.println("Enter the number of Columns");
+        int cols = scanner.nextInt();
+        System.out.println("Enter the percentage of the board with alive cells");
+        int fillPercent = scanner.nextInt();
+        Game game = new Game(rows, cols, fillPercent);
         game.start();
     }
 }
