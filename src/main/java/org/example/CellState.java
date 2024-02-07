cbfc5c46b96a339dc2b50412e65db6fada8bf00f
aapoorv2 <abhyudaya.a_ftc@external.swiggy.in>
2024-02-07 10:45:07 +0530

    added an interface for cell state (implementing state design pattern

--- src/main/java/org/example/CellState.java
+++ src/main/java/org/example/CellState.java
@@ -1,5 +1,17 @@
 package org.example;
 
+import java.util.List;
+
 public interface CellState {
-    void evolve();
+    Cell evolve(List<Cell> neighbours);
+    boolean isAlive();
+    default int countAliveNeighbours(List<Cell> neighbours) {
+        int countAlive = 0;
+        for (Cell cell : neighbours) {
+            if(cell.isAlive()) {
+                countAlive++;
+            }
+        }
+        return countAlive;
+    }
 }
